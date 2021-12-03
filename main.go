503f79a594617c708dbdeff7e5f46a7dc41197e5
somernaucl <somernaucl@github.com>
2021-12-03 08:47:54 -0500

    Changed main.go message on GET request

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		return c.HTML(http.StatusOK, "Look at this Docker GO server! v0.0.3")
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
