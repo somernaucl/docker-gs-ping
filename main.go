96407186ee943fe1fa19d7429297d911e0935001
somernaucl <somernaucl@github.com>
2021-12-05 03:26:18 -0500

    Test serving static html/css

--- main.go
+++ main.go
@@ -16,22 +16,22 @@ func main() {
         if err != nil {
            log.Fatal(err)
 }
-	e := echo.New()
+	// e := echo.New()
          
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
          
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Look at this Docker GO server! v0.0.4")
-	})
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.HTML(http.StatusOK, "Look at this Docker GO server! v0.0.4")
+	// })
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
+	// e.GET("/ping", func(c echo.Context) error {
+	// 	return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	// })
         
-	httpPort := os.Getenv("HTTP_PORT")
+	 httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
 		httpPort = "8080"
 	}
-        e.Logger.Fatal(e.Start(":" + httpPort))
+        // e.Logger.Fatal(e.Start(":" + httpPort))
 }
