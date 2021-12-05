b25ed3b3eba0778e11a00a379870bb1ade99ebcc
somernaucl <somernaucl@github.com>
2021-12-05 00:39:04 -0500

    Removed test requiring a heart <3

--- main_test.go
+++ main_test.go
@@ -27,39 +27,7 @@ import (
 // retry with exponential back-off.
 
 // Requirement 1: The application response must contain a heart ("<3")
-func TestRespondsWithLove(t *testing.T) {
 
-	pool, err := dockertest.NewPool("")
-	require.NoError(t, err, "could not connect to Docker")
-
-	resource, err := pool.Run("docker-gs-ping", "latest", []string{})
-	require.NoError(t, err, "could not start container")
-
-	t.Cleanup(func() {
-		require.NoError(t, pool.Purge(resource), "failed to remove container")
-	})
-
-	var resp *http.Response
-
-	err = pool.Retry(func() error {
-		resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("8080/tcp"), "/"))
-		if err != nil {
-			t.Log("container not ready, waiting...")
-			return err
-		}
-		return nil
-	})
-	require.NoError(t, err, "HTTP error")
-	defer resp.Body.Close()
-
-	require.Equal(t, http.StatusOK, resp.StatusCode, "HTTP status code")
-
-	body, err := io.ReadAll(resp.Body)
-	require.NoError(t, err, "failed to read HTTP body")
-
-	// Finally, test the business requirement!
-
-}
 
 // Requirement 2: The application must include a health-check end-point at /ping,
 // responding with JSON document { "Status": "OK" } if everything is well.
